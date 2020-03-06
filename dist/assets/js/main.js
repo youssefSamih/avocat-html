ff76b021144054721c919be631c789ae5b595194
youssef samih <youssef.samih97@gmail.com>
2020-03-06 17:07:21 +0100

    until complete in home

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -3,5 +3,16 @@
 var AV = AV || {};
 
 $(document).ready(function () {
-
+  $("#collapse").toggle(
+    function(){$(this).css({"color": "red"});},
+    function(){$(this).css({"color": "blue"});},
+  );
+  new fullScroll({
+    sections : 'section',
+    mainElement: 'main',
+    displayDots: true,
+    dotsPosition: 'right',
+    animateTime: 1,
+    animateFunction: 'ease'
+  });
 });
