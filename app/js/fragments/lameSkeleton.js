bdb9874a404ee6d6f56979541a67b575e662bd42
youssef samih <youssef.samih97@gmail.com>
2020-03-09 15:12:34 +0100

    finish virfication responsive

--- app/js/fragments/lameSkeleton.js
+++ app/js/fragments/lameSkeleton.js
@@ -1,36 +1,43 @@
 /*
  * lameSkeleton Functions
  * */
+"use strict"
 var url = window.location.href.split('/')[3];
 AV.lameSkeleton = {
 	init: function () {
 		$(window).on('hashchange', function(e){
 			url = window.location.href.split('/')[3];
-			$(".nav-item__link").removeClass("active");
-			$("a[href='"+ url +"']").toggleClass("active");
-		   });
-		  $(".toggle").click(function(){
-			$(".footer").toggleClass("stretch");
-			$(".footer__el").toggleClass("active");
+				$(".nav-item__link").removeClass("active");
+				$("a[href='"+ url +"']").toggleClass("active");
 		  });
 		  $(".collap").click(function(){
-			$(".sidebar").toggleClass("active");
-			$("main").toggleClass("mainActive");
-			$(".nav-item__link__title").toggleClass("collapsed");
-			$(".nav-item__link__ic").toggleClass("collapsed");
+				$(".sidebar").toggleClass("active");
+				$("main").toggleClass("mainActive");
+				$(".nav-item__link__title").toggleClass("collapsed");
+				$(".nav-item__link__ic").toggleClass("collapsed");
 			});
 			function checkPosition() {
-					if (window.matchMedia('(min-width: 768px)').matches) {
-						$("section").removeAttr("id");
-						new fullScroll({
-							sections : 'section',
-							mainElement: 'main',
-							displayDots: true,
-							dotsPosition: 'right',
-							animateTime: 1,
-							animateFunction: 'ease'
-						});
-					}
+				if (window.matchMedia('(min-width: 768px)').matches) {
+					$("section").removeAttr("id");
+					new fullScroll({
+						sections : 'section',
+						mainElement: 'main',
+						displayDots: true,
+						dotsPosition: 'right',
+						animateTime: 1,
+						animateFunction: 'ease'
+					});
+				}
+				if (window.matchMedia('(min-width: 768px)').matches) {
+					$(".toggle").click(function(){
+						$(".footer").toggleClass("stretch");
+						$(".footer__el").toggleClass("active");
+					});
+				} else {
+					$('.nav-item__link').click(function() {
+						$(".sidebar").addClass("active");
+					});
+				}
 			}
 			checkPosition();
 	}
